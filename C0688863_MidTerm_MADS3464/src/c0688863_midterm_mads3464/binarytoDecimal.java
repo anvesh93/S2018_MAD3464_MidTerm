e80138e83a26a4682d22c8a08c30375b68b87499
MacStudent <macstudent@20X-iMacXX-M-406.local>
2018-06-07 15:56:27 -0400

    reverse

--- C0688863_MidTerm_MADS3464/src/c0688863_midterm_mads3464/binarytoDecimal.java
+++ C0688863_MidTerm_MADS3464/src/c0688863_midterm_mads3464/binarytoDecimal.java
@@ -9,14 +9,6 @@ package c0688863_midterm_mads3464;
  *
  * @author macstudent
  */
-public class C0688863_MidTerm_MADS3464 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-    }
+public class binarytoDecimal {
     
 }
--- C0688863_MidTerm_MADS3464/src/c0688863_midterm_mads3464/reverse.java
+++ C0688863_MidTerm_MADS3464/src/c0688863_midterm_mads3464/reverse.java
@@ -9,6 +9,13 @@ package c0688863_midterm_mads3464;
  *
  * @author macstudent
  */
-public class reverseString {
+public class reverse {
     
+    public static void main(String[] args){
+        String input = "cestar";
+        char[] str = input.toCharArray();
+        for(int i=str, length-1; i>0; i--){
+        system.out.println(str[i]);
+    }
+    }
 }
